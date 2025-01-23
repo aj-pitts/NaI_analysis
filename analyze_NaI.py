d2bb2b45573050436391b013eee0b1fab4883867
AndrewPitts1996 <apitts4030@sdsu.edu>
2025-01-22 20:33:23 -0800

    updated to utilize config data path

--- analyze_NaI.py
+++ analyze_NaI.py
@@ -2,7 +2,7 @@ from glob import glob
 import argparse
 import subprocess
 import os
-from modules import util
+from modules import util, defaults
 
 
 def get_args():    
@@ -10,14 +10,15 @@ def get_args():
     
     parser.add_argument('galname',type=str,help='Input galaxy name.')
     parser.add_argument('bin_method',type=str,help='Input DAP patial binning method.')
-    parser.add_argument('--redshift',type=str,help='Input galaxy redshift guess.',default=None)
+    parser.add_argument('-v', '--verbose', help='Print verbose outputs. (Default: False)', action='store_true', default=False)
+    parser.add_argument('--redshift',type=str,help='Input galaxy redshift guess. (Default: None)',default=None)
     
     return parser.parse_args()
 
 def main(args):
     repodir = os.path.dirname(os.path.abspath(__file__))
-    datadir = os.path.join(repodir,'data')
-    local_outputs = os.path.join(datadir, 'local_outputs')
+    pipeline_data = defaults.get_data_path(subdir='pipeline')
+    local_data = defaults.get_data_path(subdir='local')
 
     scripts = glob('maps/*.py')
     scripts.remove('maps/__init__.py')
@@ -25,16 +26,8 @@ def main(args):
     for script in scripts:
         try:
             subprocess.run(['python', script, args.galname, args.bin_method], check=True)
-            print("########################################################################")
-            print("########################################################################")
-            print(f"##### {script} finished successfully #####")
-            print("########################################################################")
-            print("########################################################################")
         except subprocess.CalledProcessError as e:
             print(f"Error running {script}: {e}")
-
-
-    logging.info("Done.")
 
 if __name__ == "__main__":
     args = get_args()
